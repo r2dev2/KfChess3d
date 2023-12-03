d7ffbf7e0f15c99dad85684a0215b69bacdbf603
r2dev2 <ronak.badhe@gmail.com>
2023-12-02 21:10:55 -0800

    null check

--- chess.js
+++ chess.js
@@ -152,7 +152,9 @@ export class Chess extends Scene {
 
         this.picker = new MousePicker(this);
         this.picker.onClicked((obj) => {
-            console.log(obj, objMap.get(obj).toString(), 'clicked');
+            if (obj !== null) {
+                console.log(obj, objMap.get(obj).toString(), 'clicked');
+            }
         });
 
         this.initial_camera_location = Mat4.look_at(vec3(0, 10, 20), vec3(0, 0, 0), vec3(0, 1, 0));
